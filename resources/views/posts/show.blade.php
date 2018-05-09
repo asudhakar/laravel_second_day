48393d7bb039247d5ed98772e293524caa52a901
Sudhakar Annadurai <asudhakar@live.in>
2018-05-09 14:20:22 +0530

    create comment and saved successfully

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,4 +2,33 @@
 @section ('posts')
     <h2>{{ $post->title }}</h2>
     <p>{{ $post->body }}</p>
+
+    <hr>
+    <div class="comments">
+        <ul class="list-group">
+            @foreach($post->comments as $comment)
+                <li class="list-group-item">
+                    <strong>
+                        {{ $comment->created_at->diffForHumans() }}: &nbsp;
+                    </strong>
+                    {{ $comment->body }}
+                </li>    
+            @endforeach
+        </ul>
+    </div>
+    <hr>
+    <div class="card">
+        <div class="card-block">
+            <form method="POST" action="/posts/{{ $post->id }}/comments">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <textarea name="body" placeholder="Your comment here" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Add Comment</button>
+                </div>
+                @include('layouts.errors')
+            </form>
+        </div>
+    </div>
 @endsection
