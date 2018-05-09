9c83b7ff14abcc2cbbc1aa55aa600226dc616e24
Sudhakar Annadurai <asudhakar@live.in>
2018-05-09 14:24:33 +0530

    added requied in client side for browser validation

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,7 +22,7 @@
             <form method="POST" action="/posts/{{ $post->id }}/comments">
                 {{ csrf_field() }}
                 <div class="form-group">
-                    <textarea name="body" placeholder="Your comment here" class="form-control"></textarea>
+                    <textarea name="body" placeholder="Your comment here" class="form-control" required></textarea>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary">Add Comment</button>
