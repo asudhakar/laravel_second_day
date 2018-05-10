e6f911b60c97e0fc07fe8db1f7c66d69d32c56d9
Sudhakar Annadurai <asudhakar@live.in>
2018-05-10 15:36:44 +0530

    added archieves

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Carbon\Carbon;
+
+
 class PostsController extends Controller
 {
     public function __construct()
@@ -15,12 +18,30 @@ class PostsController extends Controller
 
     public function index()
     {
-        $posts = Post::latest()->get();
-        return view('posts.index', compact('posts'));
+        $posts = Post::latest();
+
+        if($month = request('month')){
+            $posts->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if($year = request('year')){
+            $posts->whereYear('created_at', $year);
+        }
+
+        $posts = $posts->get();
+
+
+        
+        return view('posts.index', compact('posts','archives'));
     }
 
     public function show(Post $post){
-        return view('posts.show', compact('post'));
+        $archives =  Post::selectRaw('year(created_at) year, monthname(created_at) month,count(*) published')
+                    ->groupBy('year', 'month')
+                    ->orderByRaw('min(created_at) desc')
+                    ->get()
+                    ->toArray();
+        return view('posts.show', compact('post','archives'));
     }
 
     public function create()
